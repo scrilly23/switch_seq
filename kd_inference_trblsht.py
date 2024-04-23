65594d40b3db5c81dc66c548d0adfd9090b97163
scrilly23 <scrilly23@gmail.com>
2024-04-23 13:58:01 -0700

    clean up dir, update plotting scripts

--- kd_inference_trblsht.py
+++ kd_inference_trblsht.py
@@ -13,12 +13,14 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import os
 import sys
+import seaborn as sns
 
 np.set_printoptions(threshold=sys.maxsize)
 mpl.rcParams['figure.dpi'] = 300
 
 ####USER DEFINED INPUTS####
-counts_table_path = '/Users/stephaniecrilly/test/count_table_ctrl.tsv'
+#counts_table_path = '/Users/stephaniecrilly/test/count_table_ctrl.tsv'
+counts_table_path = '/Users/stephaniecrilly/test/count_table.csv'
 flow_table_path = '/Users/stephaniecrilly/test/flow_table.csv'
 bounds_dict = {'bounds_log10Kd_min':-15, 'bounds_A_min':100, 'bounds_B_min':1, 'bounds_log10Kd_max':-3, 'bounds_A_max':1000000, 'bounds_B_max':100000}
 construct = 'r1-hs'
@@ -27,6 +29,7 @@ outdir = '/Users/stephaniecrilly/test'
 
 ####
 
+#counts_table = pd.read_csv(counts_table_path, sep="\t")
 counts_table = pd.read_csv(counts_table_path, sep="\t")
 flow_table = pd.read_csv(flow_table_path)
 
@@ -75,6 +78,7 @@ def compute_Kds(flow_table, counts_table):
     #get parameters needed from input tables to compute Kd
     nb_bins = flow_table.bin.nunique()
     nb_concs = flow_table.concentration.nunique()
+    flow_table.concentration_float = -1*np.log10(flow_table.concentration_float*1e12)
     concentrations = flow_table.concentration_float.unique()
     nb_genos = counts_table.shape[0]
 
@@ -95,7 +99,7 @@ def compute_Kds(flow_table, counts_table):
             cells[bb, cc] = flow_table[(flow_table.concentration == conc) 
                                         & (flow_table.bin == gate)]["cell count"].iloc[0] 
             meanfluor[bb, cc] = flow_table[(flow_table.concentration == conc)
-      n                                & (flow_table.bin == gate)]["mean_log10_APC"].iloc[0]
+                                      & (flow_table.bin == gate)]["mean_log10_APC"].iloc[0]
             stdfluor[bb, cc] = flow_table[(flow_table.concentration == conc)
                                      & (flow_table.bin == gate)]["std_log10_APC"].iloc[0]
 
@@ -105,9 +109,13 @@ def compute_Kds(flow_table, counts_table):
     print(meanfluor)
     print(stdfluor)
 
+    print('probas test')
+    probas = counts / (counts.sum(axis=1)[:, None, :])
+    print(probas)
     probas = counts / (counts.sum(axis=1)[:, None, :]) * cells[:, None, :] #calc fraction of total reads in bin for seq s, scaled by num sorted cells in that bin
-    # print(probas.shape)
-    # print(probas)
+    print(probas.shape)
+    print(probas)
+    #good up to here 20240421 2:30 pm
     probas = probas / probas.sum(axis=0)[None, :, :] #normalized over four bins for each concentration
     mean_log10_fluor = (probas * meanfluor[:, None, :]).sum(axis=0)
     #print(mean_log10_fluor)
@@ -139,8 +147,8 @@ def compute_Kds(flow_table, counts_table):
 
     return Kds, A, B, err, cov, mean_log10_fluor, std_log10_fluor, concentrations
 
-# test_concentrations = np.array([-1.00e+05, -1.00e+04, -1.00e+03, -3.16e+02, -1.00e+02,
-#  -3.16e+01, -1.00e+01, -3.16e+00, -1.00e+00, -3.16e-01])
+test_concentrations = np.array([1.00e+05, 3.16e+04, 1.00e+04, 3.16e+03, 1.00e+03, 3.16e+02, 1.00e+02,
+  3.16e+01, 1.00e+01, 3.16e+00, 1.00e+00, 3.16e-01])
 # # test_std_log10 = np.array([2.32810988, 3.90291414, 2.73507449, 2.32034969, 2.21755796, 2.71159665,
 # #  2.42383509, 2.52671346])
 # test_mean_log10 = np.array([104155, 89253, 73857, 71115, 55644, 32912,
@@ -148,11 +156,13 @@ def compute_Kds(flow_table, counts_table):
 # test_mean_log10 = np.log10(test_mean_log10)
 
 # #need to fix nans so can get as many concentrations as possible for each bin not capped at 8 concentrations
-test_Kds, test_A, test_B, test_err, test_cov, test_mean, test_std, test_conc = compute_Kds(flow_table, counts_table)
+#test_Kds, test_A, test_B, test_err, test_cov, test_mean, test_std, test_conc = compute_Kds(flow_table, counts_table)
 # print(test_Kds)                             
 # #plot a subset of the data
 # #need to rework
-# df_subset = counts_table.sample(n=4)
+#df_subset = counts_table.sample(n=5)
+df_subset = counts_table[counts_table.design == '08644_ALFA_53_7_min_2_53_bm01_loop']
+
 # xs = np.linspace(-15, -3, 100)
 
 
@@ -169,24 +179,33 @@ test_Kds, test_A, test_B, test_err, test_cov, test_mean, test_std, test_conc = c
 # plt.show()
 
 
-# # Compute Kds for the full dataset
-# #set up option to run this if above looks good
-# Kds, As, Bs, errs, covs, mean_log10_PE, std_log10_PE, concs = compute_Kds(
-#     sample_info, fluor, df)
-# df["log10Kd"] = Kds
-# df["A"] = As
-# df["B"] = Bs
-# df["r2"] = errs
-# df["sigma"] = covs
+# Compute Kds for the full dataset
+#set up option to run this if above looks good
 
-# # save the Kds values
-# df[["geno", "log10Kd", "A", "B", "r2", "sigma"] +
-#    [f"mean_log10PE{cc}" for cc in range(0, mean_log10_PE.shape[1])] +
-#    [f"std_log10PE{cc}" for cc in range(0, mean_log10_PE.shape[1])] +
-#    (["Mean fluorescence expression", "Std fluorescence expression"]
-#     if ('F' in sample_info.concentration.unique()) else [])].to_csv(
-#     snakemake.output.tsv,
-#     sep="\t", index=False)
+df = pd.DataFrame()
+Kds, As, Bs, errs, covs, mean_log10_PE, std_log10_PE, concs = compute_Kds(
+    flow_table, df_subset)
+print('output')
+print(df_subset)
+print(mean_log10_PE)
+print(mean_log10_PE[0])
+sns.scatterplot(x=test_concentrations, y=mean_log10_PE[0])
+plt.xscale('log')
+plt.show()
+plt.clf()
+df["log10Kd"] = Kds
+df["A"] = As
+df["B"] = Bs
+df["r2"] = errs
+df["sigma"] = covs
+print(df)
+
+# save the Kds values
+df[["geno", "log10Kd", "A", "B", "r2", "sigma"] +
+   [f"mean_log10PE{cc}" for cc in range(0, mean_log10_PE.shape[1])] +
+   [f"std_log10PE{cc}" for cc in range(0, mean_log10_PE.shape[1])]].to_csv(
+    f'{outdir}/all_kds.csv',
+    sep="\t", index=False)
 
 
 # with pd.option_context('mode.use_inf_as_na', True): #could be useful for negative inf where no counts
